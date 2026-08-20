6cbfe8292aa442ce3d0f844dcac2dc9e71132934
SAGNIK MONDAL <149965643+iamsagnik15@users.noreply.github.com>
2026-08-20 22:10:11 +0530

    Time: 737 ms (25.55%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1827-invalid-tweets/invalid-tweets.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+select tweet_id from Tweets where length(content)>15;
+
+
+#for counting length of the character we have to use length funcation eg. length(content)

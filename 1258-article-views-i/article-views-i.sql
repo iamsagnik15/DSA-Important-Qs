3eed8c5dabdbcb0b99e66c17e98b40544ac7aa01
SAGNIK MONDAL <149965643+iamsagnik15@users.noreply.github.com>
2026-08-20 21:26:00 +0530

    Time: 619 ms (8.87%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1258-article-views-i/article-views-i.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+select distinct author_id as id from Views
+where author_id = viewer_id
+order by author_id;
+
+
+
+# as use kora hyeche because output cheyeche id, but question e ache author id, tai print korate
+#distinct use kora hyeche jate jate single entity ase, output repeat na hoi
+# order by  clause use kora hyeche because asending order e sajajte hobe tai
